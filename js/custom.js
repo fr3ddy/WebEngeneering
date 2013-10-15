752a9af7e2b03bf67658c368ea9d7a8bb861cf57
sthuber90 <st.huber90@outlook.com>
2013-10-15 21:09:15 +0200

    Tried some stuff
    Hab underscore.js genutzt, da die Film hinzufüge immer gleich ausehen
    wird und ich das Template da praktisch fand und hab mal ausprobiert, wie
    man Bearbeite und Löschen von Filmen machen könnte. Alles nur testweise
    und leicht wieder zu entfernen versteht sich

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
-	$('#addFilm').on("click" , function(){
-		$('#finalFilm').val($('#film').val());
+	var addMovieToList = _.template('<tr><td><%- movieTitle %></td>'
+									+'<td><%- movieProducer %></td>'
+									+'<td><%- rating %></td>'
+									+'<td><button class="btn edit" title="edit"></button></td>'
+									+'<td><button class="btn delete" title="delete"></button></td></tr>');
+									
+	/*$('#addFilm').on("click" , function(){
+		$('#filmTitle').val($('#film').val());
 		$('#filmModal').modal('show');
-	});
+	});*/
 	
 	$('#saveFilm').on("click" , function(){
 		
-		$('#filmtable').append("<tr><td>"+ $('#finalFilm').val() +"</td><td>"+ $('#produzent').val() +"</td><td></td></tr>");
+		$('#filmtable').append(addMovieToList({movieTitle: $('#filmTitle').val(), movieProducer: $('#producer').val(), rating: "super"}));
 		$('#film').val("");
-		$('#finalFilm').val("");
-		$('#produzent').val("");
+		$('#filmTitle').val("");
+		$('#producer').val("");
 		$('#filmModal').modal('hide');
 	});
 });
