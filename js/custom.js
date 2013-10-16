4d3d835a7a7130d05558c1994df48e92610b6e82
fr3ddy <spamdomain@gmx.net>
2013-10-16 08:11:07 +0200

    schönere buttons

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 	var addMovieToList = _.template('<tr><td><%- movieTitle %></td>'
 									+'<td><%- movieProducer %></td>'
 									+'<td><%- rating %></td>'
-									+'<td><button class="btn edit" title="edit"></button></td>'
-									+'<td><button class="btn delete" title="delete"></button></td></tr>');
+									+'<td><button class="btn btn-sm" title="Edit"><span class="glyphicon glyphicon-pencil"></span></button></td>'
+									+'<td><button class="btn btn-sm" title="Delete"><span class="glyphicon glyphicon-trash"></span></button></td></tr>');
 									
 	/*$('#addFilm').on("click" , function(){
 		$('#filmTitle').val($('#film').val());
